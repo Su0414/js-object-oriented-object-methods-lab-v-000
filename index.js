147521780f12a2dfb9c864261fbe2333e12dbc23
Suchitra Naik <susanaik@gmail.com>
2018-07-12 17:25:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function BoardMember(name, homeState, training){
   };
 
   this.sayHi = function () {
-    return 'Hi, my name is ${this.name}. I am from New York, and I was trained in law.'
+    return 'Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.'
   };
 }
