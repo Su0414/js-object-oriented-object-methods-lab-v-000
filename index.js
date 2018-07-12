3350f8915ddc5f2989f4e4ef193644433a3fd3e0
Suchitra Naik <susanaik@gmail.com>
2018-07-12 17:22:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-}
 
-const veto = function () {
-  return 'No, I must disagree'
-};
+  veto: function () {
+    return 'No, I must disagree'
+  };
+}
